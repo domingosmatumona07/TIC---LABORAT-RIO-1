5034020f6a3f4335ae45316566571e20da7d4751
domingosmatumona07 <84391867+domingosmatumona07@users.noreply.github.com>
2021-09-20 12:55:38 +0100

    Update nSalPercentAum.c

--- ex05/nSalPercentAum.c
+++ ex05/nSalPercentAum.c
@@ -6,15 +6,15 @@ int main() {
 	double sal, newSal;
 	float perc;
 	
-	printf("Digite o sal·rio do funcion·rio: ");
+	printf("Digite o sal√°rio do funcion√°rio: ");
 	scanf("%lf", &sal);
 	printf("Digite o percentual de aumento: ");
 	scanf("%f", &perc);
-	if((sal <= 0) || (perc < 0)) {
-		printf("Dados Inv·lidos!");
+	if((sal < 0) || (perc < 0)) {
+		printf("Dados Inv√°lidos!");
 	} else {
 		newSal = sal + (sal * perc / 100);
-		printf("\nO novo sal·rio com aumento de %.2f%% È de %.2lf KZ\n", perc, newSal);
+		printf("\nO novo sal√°rio com aumento de %.2f%% √© de %.2lf KZ\n", perc, newSal);
 	}
 	return 0;
 }
