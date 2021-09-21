39721c2d1d709a05a97114483d5b078f805ee0be
domingosmatumona07 <84391867+domingosmatumona07@users.noreply.github.com>
2021-09-21 14:36:36 +0100

    Update numeroPalindromo.c

--- ex12/numeroPalindromo.c
+++ ex12/numeroPalindromo.c
@@ -5,38 +5,19 @@
 int main(void) {
 	setlocale(LC_ALL, "portuguese");
 	char num[6];
-	char aux[6];
-	char digs[11] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	int k, isNan = 0;
 	
-	printf("Digite o número: ");
+	printf("Digite o nÃºmero: ");
 	scanf("%s", num);
 	
-	int j = 0;
-	
-	while(num[j] != '\0') {
-		for(k = 0; digs[k] != '\0'; k++) {
-			if(num[j] == digs[k]) {
-				isNan++;
-				break;
-			}
-		}
-		j++;
-	}
-	
-	if(isNan == j) {
-		strcpy(aux, num);
-		strrev(num);
-	
-		int i = strcmp(aux, num);
-		
-		if(i == 0) {
-			printf("O número %s é Palíndromo.\n", aux);
-		} else {
-			printf("O número %s não é Palíndromo.\n", aux);
-		}
+	strcpy(aux, num);
+	strrev(num);
+
+	int i = strcmp(aux, num);
+
+	if(i == 0) {
+		printf("O nÃºmero %s Ã© PalÃ­ndromo.\n", aux);
 	} else {
-		printf("Digite somente números!");
+		printf("O nÃºmero %s nÃ£o Ã© PalÃ­ndromo.\n", aux);
 	}
 	return 0;
 }
