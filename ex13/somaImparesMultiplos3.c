81afa29bb39ca3d60fd2420999121d18f5487377
domingosmatumona07 <84391867+domingosmatumona07@users.noreply.github.com>
2021-09-21 14:33:28 +0100

    Update somaImparesMultiplos3.c

--- ex13/somaImparesMultiplos3.c
+++ ex13/somaImparesMultiplos3.c
@@ -5,11 +5,13 @@ int main() {
 	setlocale(LC_ALL, "portuguese");
 	int soma = 0, i;
 	
-	for(i = 1; i <= 500; i++) {
-		if((i % 2 != 0) && (i % 3 == 0)) {
+	for(i = 1; i <= 500; i += 2) {
+		if(i % 3 == 0) {
 			soma += i;
 		}
 	}
+	
+	printf("Soma = %d\n", soma);
 	
 	return 0;
 }
